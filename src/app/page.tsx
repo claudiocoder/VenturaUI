fd7b52c7a3c28a71d129c0c92e05a668859e5e7c
clcoder <clauartur98@gmail.com>
2024-10-13 21:50:04 -0600

    feat: adds mdx to render markdown documents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import CoverImage from "./components/CoverImage";
+import { getAllPostsMeta } from "./lib/mdx";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getAllPostsMeta();
+
   return (
     <div>
       <CoverImage />
+      {posts.length === 0 && (
+        <div className="mt-10 p-10">
+          <h2 className="text-2xl font-bold text-center">
+            No hay entradas disponibles
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
