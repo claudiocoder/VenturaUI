454c37ce98889dc956e9f2ebd8d8bee076d211e7
Claudio Moreno <clauartur98@gmail.com>
2024-10-13 21:52:20 -0600

    feat: adds mdx to render markdown documents

---
+++ src/app/lib/mdx/index.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import { compileMDX } from "next-mdx-remote/rsc";
+import { Blog } from "../../models/Blog";
+
+const rootDirectory = path.join(process.cwd(), "src", "content", "blog");
+export const getPostBySlug = async (slug: string) => {
+  const realSlug = slug.replace(/\.mdx$/, "");
+  const filePath = path.join(rootDirectory, `${realSlug}.mdx`);
+
+  const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
+
+  const { frontmatter, content } = await compileMDX<Blog>({
+    source: fileContent,
+    options: { parseFrontmatter: true },
+  });
+
+  return { meta: { ...frontmatter, slug: realSlug }, content };
+};
+
+export const getAllPostsMeta = async () => {
+  const files = fs.readdirSync(rootDirectory);
+
+  const posts = [];
+
+  for (const file of files) {
+    const { meta } = await getPostBySlug(file);
+    posts.push(meta);
+  }
+
+  return posts;
+};
