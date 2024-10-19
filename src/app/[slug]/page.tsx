72a172bfa993a88aa66255ae539042af26cfd18b
clcoder <clauartur98@gmail.com>
2024-10-18 23:38:30 -0600

    feat: adds title, author and date post metadata

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from "next/navigation";
 import { getPostBySlug } from "../lib/mdx";
 import CoverImage from "../components/CoverImage";
+import Image from "next/image";
+import { format, parseISO } from "date-fns";
+import { es } from "date-fns/locale";
 
 interface BlogPageProps {
   slug: string;
@@ -11,7 +14,7 @@ const getPageContent = async (slug: string) => {
 };
 
 const BlogPage = async ({ params }: { params: BlogPageProps }) => {
-  const { meta } = await getPageContent(params.slug as string);
+  const { content, meta } = await getPageContent(params.slug as string);
   if (!meta) {
     return notFound();
   }
@@ -19,11 +22,35 @@ const BlogPage = async ({ params }: { params: BlogPageProps }) => {
   return (
     <div>
       <CoverImage
-        heigth={"40vh"}
+        heigth={"30vh"}
         thumbnailUrl={meta.thumbnailUrl}
-        title="Corea"
+        title={meta.country}
         imgposition="center 40%"
       />
+
+      <div className="px-5 my-10 md:px-[20%]">
+        <h1 className="text-3xl md:text-4xl font-bold mb-5">{meta.title}</h1>
+        <div className="flex justify-between items-center mb-5">
+          <div className="flex items-center">
+            <Image
+              src={"/img/users/autor.jpg"}
+              alt={"autor"}
+              width={40}
+              height={40}
+              className="rounded-full mr-2 shadow-xl"
+            />
+            <p className="italic">Por {meta.author}</p>
+          </div>
+          <div>
+            <p className="italic font-bold">
+              {format(parseISO(meta.date as string), "MMMM dd, yyyy", {
+                locale: es,
+              })}
+            </p>
+          </div>
+        </div>
+        <div>{content}</div>
+      </div>
     </div>
   );
 };
