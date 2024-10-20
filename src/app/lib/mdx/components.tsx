e901291d557bd0201214eb04373e2cdb04583998
clcoder <clauartur98@gmail.com>
2024-10-20 15:14:39 -0600

    feat: adds back navegation to home page from post content

--- src/app/lib/mdx/components.tsx
+++ src/app/lib/mdx/components.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+
 export const MDXComponents = {
   h2: ({ ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h2 {...props} className={"text-base md:text-xl font-bold text-left"} />
   ),
+  p: ({ ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <p {...props} className={"text-base text-gray-900 text-justify"} />
+  ),
+  img: ({ ...props }) => {
+    return (
+      <Image
+        height={500}
+        width={650}
+        src={props.src}
+        alt={props.alt}
+        {...props}
+        className={
+          "mx-auto my-10 shadow-[0_5px_15px_rgba(0,0,0,0.35)] mb-2 rounded-lg"
+        }
+      />
+    );
+  },
 };
